9ac510d446ce3cf07cc54fd22968219754f04f6e
Alexandria-dr <sasha110druz@gmail.com>
2021-06-02 13:29:52 +0300

    section 3, 4

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function toggleTopMenu (){
       {header.classList.remove('is-scroll')}
       }
   window.addEventListener('scroll', toggleTopMenu);
+
+  (function(){
+    var body = document.querySelector('.features'),
+        startX = -100,
+        startY = -100,
+        w = document.documentElement.offsetWidth,
+        h = document.documentElement.offsetHeight;
+  
+      body.addEventListener('mousemove', function(evt){
+      var posX = Math.round(evt.clientX / w * startX)
+      var posY = Math.round(evt.clientY / h * startY)
+      body.style.backgroundPosition = posX + 'px ' + posY + 'px'
+    })
+  })()
